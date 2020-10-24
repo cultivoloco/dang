94061d1e4a7f9fae9f41182788241a01627c068b
Fabi <cultivoloco@gmail.com>
2020-10-24 18:02:05 -0300

    con post

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -3,6 +3,7 @@ from .serializers import TaskSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 class TaskList(APIView):
     """
@@ -15,3 +16,14 @@ class TaskList(APIView):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
+    
+    def post(self,request,format=None):
+        """
+        Create a task.
+        """
+        
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
